15b35122ebdebb347bc5525bb3dc31e0b9d20a65
KofoCole <kofodym07@yahoo.com>
2023-12-06 10:24:38 +0100

    Checked if the factorial is a positive or negative no

--- factorialOfNumbers.js
+++ factorialOfNumbers.js
@@ -3,15 +3,23 @@
 // or equal to 'n' till 1. For example, the factorial of 4 is 4 × 3 × 2 × 1
 // n! = n * (n-1) * (n-2) * (n-3) … *(n-(n-1))*.
 
+// created a function that acceptes any value
 function factorial(n) {
   let factorialProduct = 1;
 
-  //    for loop is used to calculate the factorial of a given number by iterating from the given number down to 1
+  // checked if the no is a positive or nagative number
+  if (n < 0) {
+    return " positive number and not a negative numbers.";
+  }
+
+  //  for loop is used to calculate the factorial of a given number by iterating from the given number down to 1
   for (let i = 1; i <= n; i++) {
     // ! = n * (n-1) ....
     factorialProduct = factorialProduct * i;
   }
+  // return the factorial
   return factorialProduct;
 }
 
+// output my answer
 console.log(factorial(4));
