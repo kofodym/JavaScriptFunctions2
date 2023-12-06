ce2db9306e474c6e4651a32b17d2920e572e9675
KofoCole <kofodym07@yahoo.com>
2023-12-06 09:56:50 +0100

    Added function to calculate factorial

---
+++ factorialOfNumbers.js
@@ -0,0 +1,16 @@
+// The Factorial of a whole number 'n'
+// is defined as the product of that number with every whole number less than
+// or equal to 'n' till 1. For example, the factorial of 4 is 4 × 3 × 2 × 1
+// n! = n * (n-1) * (n-2) * (n-3) … *(n-(n-1))*.
+
+function factorial(n) {
+  let factorialProduct = 1;
+
+  //    for loop is used to calculate the factorial of a given number by iterating from the given number down to 1
+  for (let i = 1; i <= n; i++) {
+    factorialProduct = factorialProduct * i;
+  }
+  return factorialProduct;
+}
+
+console.log(factorial(4));
---
+++ numPyramid.js
@@ -0,0 +1,10 @@
+function pyramid() {
+  let sum = 0;
+  let n = 10;
+  for (let i = 1; i <= 10; i++) {
+    sum += 1;
+    console.log(sum);
+  }
+}
+
+pyramid();
